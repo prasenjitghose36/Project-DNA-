436f1461793942816134e5923c4b2a2db5431015
prasenjitghose36 <prasenjitghose36@gmail.com>
2020-12-21 17:13:36 +0530

    final 1

---
+++ contact.js
@@ -0,0 +1,8 @@
+// listen for frm submit 
+document.getElementById('contactform').addEventListener('submit', submitForm);
+
+function submitForm(e){
+    e.preventDefault();
+
+    console.log(123)
+}
